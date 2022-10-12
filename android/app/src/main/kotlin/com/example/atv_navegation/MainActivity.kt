d27583a86fba7640e1ae05db503566035a20b9c9
Julia <juliamarques61@gmail.com>
2022-10-12 19:17:30 -0300

    feat: produzindo a tela de login

---
+++ android/app/src/main/kotlin/com/example/atv_navegation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.atv_navegation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/auth/sign_in_screen.dart
@@ -0,0 +1,50 @@
+import 'package:atv_navegation/src/auth/components/custom_text_field.dart';
+import 'package:flutter/material.dart';
+
+class SignScreen extends StatelessWidget {
+  const SignScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.green,
+        body: Column(
+          children: [
+            Expanded(
+                child: Container(
+              color: Colors.green,
+            )),
+            Expanded(
+                child: Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 32,
+                vertical: 40,
+              ),
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius:
+                      BorderRadius.vertical(top: Radius.circular(45))),
+              child: Column(children: const [
+                // Nome
+                // CustomTextField(
+                //   icon: Icons.name,
+                //   label: 'nome'
+                // ),
+                // Email
+                CustomTextField(
+                  icon: Icons.email,
+                  label: "Email",
+                  isObscure: false,
+                ),
+                // Senha
+                CustomTextField(
+                  icon: Icons.email,
+                  label: "Password",
+                  isObscure: true,
+                ),
+              ]),
+            )),
+          ],
+        ));
+  }
+}
